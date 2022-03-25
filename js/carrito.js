da508483c061e9c42777dbce9b3ac525ffd78eba
jualon92 <londrajuan@gmail.com>
2022-03-25 05:20:45 -0300

    lista dinamica de carrito

--- js/carrito.js
+++ js/carrito.js
@@ -1,24 +1,34 @@
-async function initCarrito(){
+let listaCarrito = [
+    { img: "/images/ham3.png", nombre: "Hamburguesa Gigante", unidades: 4, precio: 99 },
+    { img: "/images/ham3.png", nombre: "Hamburguesa Gigante", unidades: 4, precio: 99 },
+    
+]
+async function initCarrito() {
     console.warn("carrito iniciado")
-    componentHandler.upgradeDom() 
+    componentHandler.upgradeDom()
 
+    // agrego cartas contenido dinamico
+    let plantillaHbs =  await fetch('templates/productos-carrito.hbs').then(r => r.text()) 
+    var template = Handlebars.compile(plantillaHbs);
+    let html = template({ item : listaCarrito}) 
+    document.querySelector(".contenedor-resumen").innerHTML += html 
 
     //agrego listeners
-    function agregarListeners(){
+    function agregarListeners() {
         //listener ir hacia atras
         document.querySelector(".carrito-header__img").addEventListener("click", e => {
             location.hash = "menu"
         })
 
-         
+
     }
-     
+
 
     agregarListeners()
-} 
+}
 
-function quitarEsteProducto(){
+function quitarEsteProducto() {
     console.warn("quitar este ele")
-  //  quitarDeLista(this)
-   // renderProductos()
+    //  quitarDeLista(this)
+    // renderProductos()
 }
