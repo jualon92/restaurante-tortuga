2e418eea1e1344f48c97dcc93a3d217066097c2e
jualon92 <londrajuan@gmail.com>
2022-03-25 21:19:41 -0300

    carrito calcula subtotal productos

--- js/carrito.js
+++ js/carrito.js
@@ -4,14 +4,45 @@ async function initCarrito() {
     console.warn("carrito iniciado")
     componentHandler.upgradeDom()
 
-    // agrego cartas contenido dinamico
+    // HANDLEBARS, CONTENIDO DINAMICO
     let plantillaHbs =  await fetch('templates/productos-carrito.hbs').then(r => r.text()) 
     var template = Handlebars.compile(plantillaHbs);
    //let listaActual  = JSON.parse(window.localStorage.getItem("listaPedidos"))
     let html = template({ item : listaCarrito}) 
     document.querySelector(".contenedor-resumen").innerHTML += html 
+    
 
-    //agrego listeners
+    // PRECIO X UNIDADES
+    let listaCartas = document.querySelectorAll(".carrito-cartas")
+    listaCartas.forEach(carta => {
+        console.warn("inii")
+      //  console.warn(carta.querySelector(".carrito-cartas__numero-precio"))
+        let valorIndividual = parseFloat(carta.querySelector(".carrito-cartas__numero-precio").innerHTML)
+        let unidades = parseInt(carta.querySelector(".unidad").innerHTML)
+        let valorTotal = valorIndividual * unidades
+        console.log("Unidades", unidades)
+        console.log("valor inidivual", valorIndividual)
+        console.log("suma", valorTotal)
+      carta.querySelector(".carrito-cartas__numero-precio").innerHTML = valorTotal
+      
+    });
+
+
+    /// ACUMULADOR DE TOTAL
+    let acuTotal = 0
+    let listaPrecios =   document.querySelectorAll(".carrito-cartas__numero-precio")
+    listaPrecios.forEach(ele => {
+        const suma = parseFloat(ele.innerHTML)
+        console.warn(suma)
+        acuTotal +=  suma
+    });
+    //calcular total
+    document.querySelector(".carrito-total__resultado").innerHTML = acuTotal
+
+
+
+
+    // SET LISTENERS
     function agregarListeners() {
         //listener ir hacia atras
         document.querySelector(".carrito-header__img").addEventListener("click", e => {
--- js/productos.js
+++ js/productos.js
@@ -6,7 +6,7 @@ let burgerList = [
         nombre: "Burger Doble Cheddar",
         descripcion: "Super Giga Cheddar-ium",
         imagen: "images/burgerGigante.png",
-        precio: `$ 12.50`,
+        precio: `12.50`,
         texto: `La densidad del cheddar utilizado es dos veces el encontrado en
     la
     usual hamburguesa`,
@@ -20,7 +20,7 @@ let burgerList = [
         nombre: "Burger Dancing Chicken",
         descripcion: "Pollo bailarin",
         imagen: "/images/hamGrande.avif",
-        precio: `$ 11.25`,
+        precio: `11.25`,
         texto: `Un pollo alegre es un sabor diferente`,
         estrellas: 4.2,
         calorias: "130kcal",
@@ -31,7 +31,7 @@ let burgerList = [
         nombre: "Tower Burger MAX",
         descripcion: "Hamburguesa de multiples pisos",
         imagen: "/images/towerburger-grande.png",
-        precio: `$ 26.25`,
+        precio: `26.25`,
         texto: `Hamburguesa recomendada para mas de una persona, procurar posicionarla en el medio de la mesa para no causar una desgracia`,
         estrellas: 5,
         calorias: "250kcal",
