86fd25b1808d486bffd19ff8e7d1247dd662307f
jualon92 <londrajuan@gmail.com>
2022-03-29 07:46:32 -0300

    cache first

--- sw.js
+++ sw.js
@@ -63,7 +63,7 @@ self.addEventListener("activate", e => {
 
  
 
-
+/*
 self.addEventListener('fetch', (e) => {
     e.respondWith((async () => {
       const r = await caches.match(e.request);
@@ -77,6 +77,14 @@ self.addEventListener('fetch', (e) => {
     })());
   });
 
+*/
 
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
   
  
